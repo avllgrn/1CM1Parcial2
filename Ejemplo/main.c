9def4216b45ac695f9e0368844d82df51246fbc8
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-04-12 14:51:05 -0600

    Area y perimetro de circulo, condicionadas

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -3,35 +3,24 @@
 #include <math.h>
 
 int main(void){
-    float a, b, c, x1, x2, disc;
+    float r,d,a,p;
 
-    printf("Ingresa a ");
-    scanf("%f",&a);
-    printf("Ingresa b ");
-    scanf("%f",&b);
-    printf("Ingresa c ");
-    scanf("%f",&c);
+    printf("Ingresa radio ");
+    scanf("%f",&r);
 
-    disc = pow(b,2)-4*a*c;
 
-    if(a == 0){
-        printf("Error! Indeterminacion...\n\n");
+    if(r < 0){
+        printf("Error! radio invalido...\n\n");
     }
     else{
-        if(disc < 0){
-            printf("Error! Raices imaginarias...\n\n");
-        }
-        else{
-            x1 = (-b+sqrt(disc))/(2*a);
-            x2 = (-b-sqrt(disc))/(2*a);
-            printf("x1 = %f\n",x1);
-            printf("x2 = %f\n",x2);
-        }
+        d = 2 * r;
+        a = M_PI * pow(r,2);
+        p = 2 * M_PI * r;
+        printf("Radio = %f\n",r);
+        printf("Diametro = %f\n",d);
+        printf("Area = %f\n",a);
+        printf("Perimetro = %f\n",p);
     }
 
     return 0;
-}
-
-int esMultiploDe(int x, int y){
-    return x%y == 0;
 }
