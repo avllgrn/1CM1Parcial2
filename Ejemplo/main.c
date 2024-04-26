b245274f402b6778c8c05e8d2377344cd83355c5
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-04-26 10:58:27 -0600

    Genero con 1 estructura anidadas if-else if-else

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -7,23 +7,17 @@ int main(void){
     if(genero == 'f'){
         printf("femenino\n");
     }
+    else if(genero == 'F'){
+        printf("Femenino\n");
+    }
+    else if(genero == 'm'){
+        printf("masculino\n");
+    }
+    else if(genero == 'M'){
+        printf("Masculino\n");
+    }
     else{
-        if(genero == 'F'){
-            printf("Femenino\n");
-        }
-        else{
-            if(genero == 'm'){
-                printf("masculino\n");
-            }
-            else{
-                if(genero == 'M'){
-                    printf("Masculino\n");
-                }
-                else{
-                    printf("Genero no reconocido\n");
-                }
-            }
-        }
+        printf("Genero no reconocido\n");
     }
 
     return 0;
