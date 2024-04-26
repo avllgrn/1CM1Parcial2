bcbd0022c6236003b011ed6e1793a005c3a292ee
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-04-26 11:12:54 -0600

    Estado civil con 1 estructura anidadas if-else if-else

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -7,43 +7,29 @@ int main(void){
     if(estadoCivil == 's'){
         printf("soltero\n");
     }
+    else if(estadoCivil == 'S'){
+        printf("Soltero\n");
+    }
+    else if(estadoCivil == 'c'){
+        printf("casado\n");
+    }
+    else if(estadoCivil == 'C'){
+        printf("Casado\n");
+    }
+    else if(estadoCivil == 'd'){
+        printf("divorciado\n");
+    }
+    else if(estadoCivil == 'D'){
+        printf("Divorciado\n");
+    }
+    else if(estadoCivil == 'v'){
+        printf("viudo\n");
+    }
+    else if(estadoCivil == 'V'){
+        printf("Viudo\n");
+    }
     else{
-        if(estadoCivil == 'S'){
-            printf("Soltero\n");
-        }
-        else{
-            if(estadoCivil == 'c'){
-                printf("casado\n");
-            }
-            else{
-                if(estadoCivil == 'C'){
-                    printf("Casado\n");
-                }
-                else{
-                    if(estadoCivil == 'd'){
-                        printf("divorciado\n");
-                    }
-                    else{
-                        if(estadoCivil == 'D'){
-                            printf("Divorciado\n");
-                        }
-                        else{
-                            if(estadoCivil == 'v'){
-                                printf("viudo\n");
-                            }
-                            else{
-                                if(estadoCivil == 'V'){
-                                    printf("Viudo\n");
-                                }
-                                else{
-                                    printf("Estado civil no reconocido\n");
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        printf("Estado civil no reconocido\n");
     }
 
     return 0;
