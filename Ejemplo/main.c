55758a00b3a1779f7d180e97cebe81f99d9d65d8
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-04-29 20:16:57 -0600

    Edades con operadores logicos

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
 int main(void){
 
-    printf("Operador && AND\n\n");
+    int edad;
 
-    printf("%d && %d => %d\n",0,0,0&&0);
-    printf("%d && %d => %d\n",0,1,0&&1);
-    printf("%d && %d => %d\n",1,0,1&&0);
-    printf("%d && %d => %d\n",1,1,1&&1);
+    printf("Ingresa tu edad ");
+    scanf("%d",&edad);
 
-    printf("\n\n");
-
-    printf("Operador && OR\n\n");
-
-    printf("%d || %d => %d\n",0,0,0||0);
-    printf("%d || %d => %d\n",0,1,0||1);
-    printf("%d || %d => %d\n",1,0,1||0);
-    printf("%d || %d => %d\n",1,1,1||1);
-
-    printf("\n\n");
-
-    printf("Operador ! NOT\n\n");
-
-    printf("!%d => %d\n",0,!0);
-    printf("!%d => %d\n",1,!1);
-    printf("\n\n");
+    if(edad < 0){
+        printf("Error! Edad negativa.");
+    }
+    else if(0<=edad && edad<18){
+        printf("Eres menor de edad.");
+    }
+    else if(18<=edad && edad<120){
+        printf("Eres mayor de edad.");
+    }
+    else{
+        printf("Error! Edad positiva.");
+    }
 
     return 0;
 }
